abb44e3229a8024fcf0d1ce952ed74245af3ea0e
Ebert Maximilian <s2510567006@fhooe.at>
2025-10-25 15:50:04 +0200

    Implement TaskLed

--- Uebung1/Template_Uebung1/src/Tasks/TaskLed.c
+++ Uebung1/Template_Uebung1/src/Tasks/TaskLed.c
@@ -8,13 +8,51 @@
   ******************************************************************************
   */
 	
-#include "BSP/TftDisplay.h"
 #include "StdDef.h"
 #include "TaskLed.h"
+#include "BSP/Led.h"
 
+// Define LED task states
+typedef enum {
+    LED_STATE_START,
+    LED_STATE_LED3_ON,
+    LED_STATE_LED4_ON,
+    LED_STATE_LED5_ON
+} LED_STATE_t;
 
 void TaskLed (void)
 {
-  
-	Tft_DrawString(10, 18+2*24, "Led ");	
+ 
+    static LED_STATE_t state = LED_STATE_START;
+
+    // Simple state machine
+    switch (state)
+    {
+        case LED_STATE_LED3_ON:
+            Led_TurnOn(LEDType_LED3);
+            Led_TurnOff(LEDType_LED4);
+            Led_TurnOff(LEDType_LED5);
+            state = LED_STATE_LED4_ON;
+            break;
+
+        case LED_STATE_LED4_ON:
+            Led_TurnOff(LEDType_LED3);
+            Led_TurnOn(LEDType_LED4);
+            state = LED_STATE_LED5_ON;
+            break;
+
+        case LED_STATE_LED5_ON:
+            Led_TurnOff(LEDType_LED4);
+            Led_TurnOn(LEDType_LED5);
+            state = LED_STATE_LED3_ON;
+            break;
+        case LED_STATE_START: // fall through to default
+        default:
+            Led_TurnOff(LEDType_LED3);
+            Led_TurnOff(LEDType_LED4);
+            Led_TurnOff(LEDType_LED5);
+            state = LED_STATE_LED3_ON;
+            break;
+    }
 }
+
